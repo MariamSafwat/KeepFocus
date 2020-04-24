4808573e8460e737d6ebe17dd1c9ac0ea137ac68
Mariam Safwat <mariamsafwat65@yahoo.com>
2020-04-24 03:09:30 +0200

    Add Bar chart widget

---
+++ gui/myBar.py
@@ -0,0 +1,57 @@
+from PyQt5 import QtGui , QtWidgets
+from PyQt5.QtChart import QChart, QChartView, QValueAxis, QBarCategoryAxis, QBarSet, QBarSeries
+from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QVBoxLayout 
+
+class BarChart(QtWidgets.QWidget):
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+
+        self.widget = QWidget(self)
+        self.widget.setObjectName("main")
+        self.widget.setStyleSheet("""
+                    QWidget#main{
+                        border:1px solid #148CD2;
+                        border-radius:10px;
+                    }
+        """)
+        
+        data0 = [1,6,3,2,8,6,7]
+        data1 = [5,8,2,6,1,2,4]
+        
+        
+        self.create_barchart(data0,data1)
+
+        layout = QVBoxLayout (self)
+        layout.setContentsMargins(20,20,20,20)
+        layout.addWidget(self.chartview)
+        self.widget.setLayout(layout)
+        self.widget.setFixedSize(400,300)
+        
+    def create_barchart(self, data0, data1):
+        set0 = QBarSet('Productive')
+        set1 = QBarSet('Destructive')
+        set0.append(data0)
+        set1.append(data1)
+        series = QBarSeries()
+        series.append(set0)
+        series.append(set1)
+        chart = QChart()
+        chart.addSeries(series)
+        chart.setTitle('Weekly Bar Chart')
+        chart.setAnimationOptions(QChart.SeriesAnimations)
+        days = ('Mon', 'Teu', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+        axisX = QBarCategoryAxis()
+        axisX.append(days)
+        axisY = QValueAxis()
+        axisY.setRange(0, 24)
+        chart.addAxis(axisX, Qt.AlignBottom)
+        chart.addAxis(axisY, Qt.AlignLeft)
+        chart.legend().setVisible(True)
+        chart.legend().setAlignment(Qt.AlignBottom)
+        self.chartview = QChartView(chart)
+        #self.setCentralWidget(chartView)
+        self.chartview.setRenderHint(QPainter.Antialiasing)
+        
+    
