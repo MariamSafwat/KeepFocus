49a4fbae74a9921e3c6c2ac2a19be89254443ef7
minaaaatef <mina.atef0@gmail.com>
2020-04-29 19:20:12 +0200

    implement TakeDecision

--- controller.py
+++ controller.py
@@ -1,15 +1,43 @@
 from database.modelsfun import *
 from backend import *
+from backend.system_api import *
 
-def main():
+
+def TakeDecision(timerison) :
+    last10min = session.query(Programsdata).order_by(a.id.desc()).limit(10)
+    
+    productiveCount = 0
+    for min in last10min:
+        if min.productive == Programsdata.DISTRACTIVE:
+            productiveCount +=1
+    
+    if productiveCount > 6 and timerison:
+        sendNotification("Keep Focus","Shouldn't you be working !?")
+
+
+
+def Classifier():
+    screenshot = takeScreenshot()
+    screenshot.save('images/current.png')
 
     progTexts = returnProgramTexts()
-    print(extract_txt('images/Shot4.png',progTexts))
+    text_dic = extract_txt('images/current.png',progTexts)
 
     progImgs = returnProgramImages()
-    print(getmatches('images/Screenshot (56).png', progImgs))
-    print("---------------------------")
-    print(progImgs)
+    img_dic = getmatches('images/current.png', progImgs)
 
+    sum_dic = {}
+    for key in text_dic.keys():
+        sum_dic[key] = text_dic[key] + img_dic[key]
 
-main()
+    orderedTuplesOfText = sorted(text_dic.items() ,  key=lambda x: -x[1]  )
+    most_text = orderedTuplesOfText[0][0]
+
+    orderedTuplesOfImg = sorted(img_dic.items() ,  key=lambda x: -x[1]  )
+    most_img = orderedTuplesOfImg[0][0]
+
+    orderedTuplesOfTuple = sorted(sum_dic.items() ,  key=lambda x: -x[1]  )
+    most_Sum = orderedTuplesOfImg[0][0]
+
+    
+    return most_Sum
