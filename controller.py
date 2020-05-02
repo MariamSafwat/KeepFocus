6482f4a20163f772ac6b654ad523724757cb0863
Nader Nabil <nadernabilmansour@gmail.com>
2020-05-02 03:42:10 +0200

    controller - normalize dict values

--- controller.py
+++ controller.py
@@ -15,30 +15,60 @@ def TakeDecision() :
         sendNotification("Keep Focus","Shouldn't you be working !?")
 
 
-
 def Classifier(timerison):
     screenshot = takeScreenshot()
     screenshot.save('images/current.png')
 
     progTexts = returnProgramTexts()
     text_dic = extract_txt('images/current.png',progTexts)
+    # print("--------------------------------------")
+    # print(text_dic)
+    # print("--------------------------------------")
 
-    progImgs = returnProgramImages()
-    img_dic = getmatches('images/current.png', progImgs)
+    #normalize(text_dic)
 
-    sum_dic = {}
-    for key in text_dic.keys():
-        sum_dic[key] = text_dic[key] + img_dic[key]
+    # print(text_dic)
+    # print("--------------------------------------")
+
+    # progImgs = returnProgramImages()
+    # img_dic = getmatches('images/current.png', progImgs)
+    # print("--------------------------------------")
+    # print(img_dic)
+    # print("--------------------------------------")
+
+    # normalize(img_dic)
+
+    # print(img_dic)
+    # print("--------------------------------------")
+
+    # sum_dic = {}
+    # for key in text_dic.keys():
+    #     sum_dic[key] = text_dic[key] + img_dic[key]
 
     orderedTuplesOfText = sorted(text_dic.items() ,  key=lambda x: -x[1]  )
     most_text = orderedTuplesOfText[0][0]
 
-    orderedTuplesOfImg = sorted(img_dic.items() ,  key=lambda x: -x[1]  )
-    most_img = orderedTuplesOfImg[0][0]
+    # orderedTuplesOfImg = sorted(img_dic.items() ,  key=lambda x: -x[1]  )
+    # most_img = orderedTuplesOfImg[0][0]
 
-    orderedTuplesOfTuple = sorted(sum_dic.items() ,  key=lambda x: -x[1]  )
-    most_Sum = orderedTuplesOfImg[0][0]
-    
-    updateOnScreenshoot(timerison,most_Sum)
+    # orderedTuplesOfTuple = sorted(sum_dic.items() ,  key=lambda x: -x[1]  )
+    # most_Sum = orderedTuplesOfImg[0][0]
+
+    updateOnScreenshoot(timerison,most_text)
     TakeDecision()
-    
+
+
+def normalize (dict_x):
+    dict_values = dict_x.values()
+    dict_min = min(dict_values)
+    dict_max = max(dict_values)
+
+    if dict_max == 0:
+        return 
+
+    for key, value in dict_x.items():
+        if dict_max == dict_min : 
+            dict_x[key] = 1 
+        else:
+            dict_x[key] = ( (dict_x[key] - dict_min) / (dict_max - dict_min) )
+
